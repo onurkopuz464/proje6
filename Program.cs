e50c207136ec0debbc6a67b11809bd10a08798d2
onurkopuz464 <onurkopuz464@gmail.com>
2022-10-22 16:02:05 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace c
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+          switch (month)
+          {
+            case 10:
+            Console.WriteLine("Ocak");
+            break;
+            case 2:
+            Console.WriteLine("Şubat");
+            break;
+            case 3:
+            Console.WriteLine("Mart");
+            break;
+            default:
+              Console.WriteLine("Hata");
+              break;
+
+          }
+          switch (month)
+          {
+            case 10:
+            case 1:
+            case 2:
+              Console.WriteLine("Kış");
+              break;
+            case 6:
+            case 7:
+            Console.WriteLine("Yaz");
+              break;
+            default:
+            Console.WriteLine("Hata");
+            
+            break;
+          }
+
+         
+        
+
+        }
+        
+      
+    }
+    
+}
